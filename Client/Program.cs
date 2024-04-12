e506daf7de6c199928e8803b5bc17475557b4d23
Ajeszny <245107@edu.p.lodz.pl>
2024-04-12 16:04:23 +0200

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetMQ.Sockets;
+using NetMQ;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (RequestSocket client = new RequestSocket())
+            {
+                try
+                {
+                    client.Connect("tcp://127.0.0.1:1337");
+                    Console.WriteLine("Enter your message: ");
+                    String msg = Console.ReadLine();
+                    client.SendFrame(msg);
+                    Console.WriteLine(client.ReceiveFrameString());
+                    long zalupa = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+                    client.SendFrame(zalupa.ToString());
+                    String tstamp = client.ReceiveFrameString();
+                    Console.WriteLine("Recieved: {0}", tstamp);
+                    client.Close();
+                } catch (Exception e)
+                {
+                    Console.WriteLine("Server not running!" + e.Message);
+                }
+            }
+            while (true)
+            {
+
+            }
+        }
+    }
+}
---
+++ PO2_7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using NetMQ.Sockets;
+using NetMQ;
+
+namespace PO2_7
+{
+    internal class Program
+    {
+        static void ServeClient(ResponseSocket serv)
+        {
+            string msg = serv.ReceiveFrameString();
+            serv.SendFrame("Message acknowledged, proceeding to read the timestamp!");
+            string tstamp = serv.ReceiveFrameString();
+            Console.WriteLine("Message: " + msg + "\nTimestamp: " + tstamp);
+            long zalupa =  DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            serv.SendFrame(zalupa.ToString());
+        }
+       
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (ResponseSocket serv = new ResponseSocket())
+                {
+                    serv.Bind("tcp://*:1337");
+
+                    while (true)
+                    {
+                        ServeClient(serv);
+                    }
+                }
+
+            } catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
